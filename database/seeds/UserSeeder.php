5db99cdaaff26c12610777741c08bad38e812d9f
GabrielAttila <gabrieljmorenot@gmail.com>
2018-12-09 21:22:19 -0400

    13 - Constructor de consultas SQL de Laravel

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,11 +12,17 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        //$professions = DB::select('SELECT id FROM professions WHERE title = ?', ['Desarrollador back-end']);
+
+        $professionId = DB::table('professions')
+            ->whereTitle('title', 'Desarrollador back-end')
+            ->value('id');
+
         DB::table('users')->insert([
             'name' => 'GabrielAttila',
             'email' => 'gabrieljmorenot@gmail.com',
             'password' => bcrypt('123'),
-            //'profession_id' =>
+            'profession_id' => $professionId,
         ]);
     }
 }
