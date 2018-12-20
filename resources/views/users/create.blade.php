d17be42aa0acafdcb07d76e5e2b9961d2712596b
GabrielAttila <gabrieljmorenot@gmail.com>
2018-12-20 15:42:43 -0400

    28 - Creación de formulario para agregar usuarios con Laravel

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -5,8 +5,18 @@
 @section('content')
     <h1>Crear usuario</h1>
 
-    <form method="POST" action="{{ url('usuarios/crear') }}">
+    <form method="POST" action="{{ url('usuarios') }}">
         {!! csrf_field() !!}
+
+        <label for="name">Nombre:</label>
+        <input type="text" name="name" id="name" placeholder="Gabriel Moreno">
+        <br>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" placeholder="gabriel@example.com">
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" placeholder="Mayor a 6 caracteres">
+        <br>
         <button type="submit">crear usuario</button>
     </form>
 
